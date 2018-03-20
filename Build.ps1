85c2a0380035fc7cf590a5a13bfb784a0dcaee2e
Benjamin Moles <bmoles@clearpeople.com>
2018-03-20 15:13:36 +0000

    Fix issues in Build.ps1 file

--- Build.ps1
+++ Build.ps1
@@ -3,9 +3,8 @@ $VSDevEnvPath = "c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\
 
 
 docker build -t sitecore-8-base .\images\sitecore-8-base
-
-&$VSDevEnvPath /build .\Sitecore82TDS.sln Release
-#&"c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" /build .\Sitecore82TDS.sln Release
-#& "c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" /build "c:\Docker\Sitecore82TDS\Sitecore82TDS.sln" Release
-
 docker build -t sitecore:8.2.161221 .\images\sitecore-82rev161221
+
+& $VSDevEnvPath /build .\Sitecore82TDS.sln Debug
+
+& $VSDevEnvPath /build .\Sitecore82TDS.sln Release
